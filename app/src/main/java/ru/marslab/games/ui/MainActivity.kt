d135b135bfb36d15e16ce65dbf7ad395b50389c9
Aristman <armobila@yandex.ru>
2021-04-12 03:26:17 +0300

    First work game version

--- app/src/main/java/ru/marslab/games/ui/MainActivity.kt
+++ app/src/main/java/ru/marslab/games/ui/MainActivity.kt
@@ -1,10 +1,40 @@
 package ru.marslab.games.ui
 
 import android.os.Bundle
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import ru.marslab.games.R
 
-class MainActivity: AppCompatActivity() {
+const val INIT_POSITION = 0.5f
+const val GAME_STEP = 0.01f
+
+class MainActivity : AppCompatActivity() {
+    private var brakeLine: ConstraintSet = ConstraintSet()
+    private lateinit var gameField: ConstraintLayout
+    private var position: Float = INIT_POSITION
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initGame()
     }
+
+    private fun initGame() {
+        brakeLine.clone(applicationContext, R.layout.activity_main)
+        gameField = findViewById(R.id.gameField)
+        findViewById<FrameLayout>(R.id.upField).setOnClickListener {
+            position += GAME_STEP
+            brakeLine.setGuidelinePercent(R.id.brakeLine, position)
+            brakeLine.applyTo(gameField)
+        }
+        findViewById<FrameLayout>(R.id.downField).setOnClickListener {
+            position -= GAME_STEP
+            brakeLine.setGuidelinePercent(R.id.brakeLine, position)
+            brakeLine.applyTo(gameField)
+        }
+    }
+
+
 }
