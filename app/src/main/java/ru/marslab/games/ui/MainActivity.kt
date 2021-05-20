3eb39a1e3828cca9be2d9388256a2f142301d0de
Aristman <armobila@yandex.ru>
2021-05-20 12:12:02 +0300

    Second Thread. Make app with callback flow

--- app/src/main/java/ru/marslab/games/ui/MainActivity.kt
+++ app/src/main/java/ru/marslab/games/ui/MainActivity.kt
@@ -1,16 +1,15 @@
 package ru.marslab.games.ui
 
+import android.graphics.Canvas
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
 import ru.marslab.games.R
 
 const val INIT_POSITION = 0.5f
@@ -30,10 +29,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initGame()
-        when (game()) {
-            1 -> Toast.makeText(applicationContext, "верх!!!", Toast.LENGTH_SHORT).show()
-            2 -> Toast.makeText(applicationContext, "Низ!!!", Toast.LENGTH_SHORT).show()
-        }
+        game()
     }
 
     private fun initGame() {
@@ -49,28 +45,24 @@ class MainActivity : AppCompatActivity() {
                 downFrame.setOnClickListener(null)
             }
         }
-//        position.observe(this) {
-//            brakeLine.setGuidelinePercent(R.id.brakeLine, position.value!!)
-//            brakeLine.applyTo(gameField)
-//        }
-//        brakeLine.clone(applicationContext, R.layout.activity_main)
-
-    }
-
-    private fun game(): Int {
         GlobalScope.launch {
             clickCallbackFlow.collect {
                 position += it
             }
         }
-        while (true) {
-            brakeLine.apply {
-                setGuidelinePercent(R.id.brakeLine, position)
-                applyTo(gameField)
+    }
+
+    private fun game() {
+        brakeLine.clone(applicationContext, R.layout.activity_main)
+        GlobalScope.launch {
+            while (true) {
+                withContext(Dispatchers.Main) {
+                    brakeLine.setGuidelinePercent(R.id.brakeLine, position)
+                    brakeLine.applyTo(gameField)
+                }
+                println(position)
+                delay(50)
             }
-            gameField.refreshDrawableState()
-            if (position <= 0) return 1
-            if (position > 1) return 2
         }
     }
 
